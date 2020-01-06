0699305c3a73e1331d7e98de82b2f6ea0f5451fc
Jurabek Tolmasov <tolmasovjurabek@gmail.com>
2020-01-06 09:55:24 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -6,23 +6,7 @@ PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
-def assembled_aoh 
-  
-#     DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
-# JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
-# PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
-# KATE_G = { name:  "Kate Gompert", occupation: "None" }
-# BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
-       
-
-# ALL_CAPS = [
-#   DON_G,
-#   JOELLE_VD,
-#   PAT_M,
-#   KATE_G,
-#   BRUCE_G
-#   ]
-   [
+def assembled_aoh = [
      { :name=>"Don Gately", :occupation=> "Live-in Staff" },
 { :name=>"Joelle van Dyne", :occupation=> "Radio Personality" },
 { :name=>"Pat Monteseian", :occupation=> "Staff" },
