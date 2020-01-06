b76a576fba80319ebd30c3f62ed7e0d942dd18af
Jurabek Tolmasov <tolmasovjurabek@gmail.com>
2020-01-06 09:52:44 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -22,7 +22,7 @@ def assembled_aoh
 #   KATE_G,
 #   BRUCE_G
 #   ]
-   ALL_CAPS = [
+   [
      { :name=>"Don Gately", :occupation=> "Live-in Staff" },
 { :name=>"Joelle van Dyne", :occupation=> "Radio Personality" },
 { :name=>"Pat Monteseian", :occupation=> "Staff" },
